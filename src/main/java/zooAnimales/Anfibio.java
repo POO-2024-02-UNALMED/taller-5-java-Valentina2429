93d0604ae0a5c633b6887265ae99628e5212e031
Valentina2429 <vleonb@unal.edu.co>
2024-12-09 22:49:54 -0500

    2° intento

--- src/main/java/zooAnimales/Anfibio.java
+++ src/main/java/zooAnimales/Anfibio.java
@@ -1,19 +1,67 @@
 package zooAnimales;
 
+import java.util.ArrayList;
+
 public class Anfibio extends Animal {
-    private Anfibio[] listado;
-    public int ranas;
-    public int salamandras;
-    private String colorPiel;
-    private Boolean venenoso;
+    private static ArrayList<Anfibio> listado=new ArrayList<>();
+	public static int ranas;
+	public static int salamandras;
+	private String colorPiel;
+	private boolean venenoso;
+	
+	public Anfibio() {
+		this(null,0,null,null,null,false);
+	}
+	
+	public Anfibio(String nombre, int edad, String habitat, String genero, String colorPiel, boolean venenoso) {
+		super(nombre,edad,habitat,genero);
+		this.colorPiel=colorPiel;
+		this.venenoso=venenoso;
+		Anfibio.listado.add(this);
+	}
+	
+	
+	public static void setListado(ArrayList<Anfibio> listado) {
+		Anfibio.listado=listado;
+	}
+	public static ArrayList<Anfibio> getListado() {
+		return listado;
+	}
+	
+	public void setColorPiel(String colorPiel) {
+		this.colorPiel=colorPiel;
+	}
+	public String getColorPiel() {
+		return colorPiel;
+	}
+	
+	public void setVenenoso(boolean venenoso) {
+		this.venenoso=venenoso;
+	}
+	public boolean isVenenoso() {
+		return venenoso;
+	}
+	
+	public int cantidadAnfibios() {
+		return Anfibio.listado.size();
+		
+	}
+	
+	public String movimiento() {
+		return "saltar";
+	}
+	
+	public static Anfibio crearRana(String nombre, int edad, String genero) {
+		Anfibio Anfibio =new Anfibio(nombre,edad, "selva",genero, "rojo", true);
+		Anfibio.ranas++;
+		return Anfibio;
+	}
+	
+	public static Anfibio crearSalamandra(String nombre, int edad, String genero) {
+		Anfibio Anfibio=new Anfibio(nombre, edad, "selva",genero, "negro y amarillo", false);
+		Anfibio.salamandras++;
+		return Anfibio;
+		
+	}
 
-    public void cantidadAnfibios(){
-
-    }
-    public void movimiento(){}
-
-    public void crearRana(){}
-
-    public void crearSalamandra(){}
-    
 }
--- src/main/java/zooAnimales/Animal.java
+++ src/main/java/zooAnimales/Animal.java
@@ -1,22 +1,96 @@
 package zooAnimales;
 
+import java.util.ArrayList;
+
 public class Animal {
     private static int totalAnimales;
-    private String nombre;
-    private int edad;
-    private String habitat;
-    private String genero;
-    private Zona[1] zona;
+	private String nombre;
+	private int edad;
+	private String habitat;
+	private String genero;
+	private Zona zona;
+	
+	public Animal() {
+		this(null,0,null,null);
+	}
+	
+	public Animal( String nombre, int edad, String habitat, String genero) {
+		this.nombre=nombre;
+		this.edad=edad;
+		this.habitat=habitat;
+		this.genero=genero;
+		this.zona=null;
+		totalAnimales++;
+	
+	}
+	
+	
+	public static void setTotalAnimales(int totalAnimales) {
+		Animal.totalAnimales=totalAnimales;
+	}
+	public static int getTotalAnimales() {
+		return totalAnimales;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre=nombre;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setEdad(int edad) {
+		this.edad=edad;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	
+	public void setHabitat(String habitat) {
+		this.habitat=habitat;
+	}
+	public String getHabitat() {
+		return habitat;
+	}
+	
+	public void setGenero(String genero) {
+		this.genero=genero;
+	}
+	public String getGenero() {
+		return genero;
+	}
+	
+	public void setZona(Zona zona) {
+		this.zona=zona;
+	}
+	public Zona getZona() {
+		return zona;
+	}
+	
+	
+	public String movimiento() {
+		return "desplazarse";
+	}
+	
+	public static String totalPorTipo() {
+		
+		return "Mamiferos: "+Mamifero.getListado().size()+"\n"+ 
+		"Aves: "+Ave.getListado().size()+"\n"+ 
+		"Reptiles: "+Reptil.getListado().size()+"\n" + 
+		"Peces: " +Pez.getListado().size()+"\n"+ 
+		"Anfibios: "+Anfibio.getListado().size();
+		
+		
+	}
+	
+	public String toString() {
+		if (getZona()!=null) {
+			return "Mi nombre es "+ getNombre()+", tengo una edad de " +getEdad()+ ", habito en "+getHabitat()+" y mi genero es "+getGenero()+", la zona en la que me ubico es "+getZona().getNombre()+", en el "+getZona().getZoo().getNombre();
+		}
+		else {
+		    return "Mi nombre es "+ getNombre()+", tengo una edad de " +getEdad()+ ", habito en "+getHabitat()+" y mi genero es "+getGenero();
+		}
+	}
 
-    public void movimiento(){
 
-    }
-
-    public void totalPorTipo(){
-
-    }
-
-    public String toString(){
-        return this.nombre
-    }
 }
