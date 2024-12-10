3fe458a157ac7d3c47a4c06414bc3d6b02e7474d
Valentina2429 <vleonb@unal.edu.co>
2024-12-09 22:48:14 -0500

    Update Reptil.java

--- src/main/java/zooAnimales/Reptil.java
+++ src/main/java/zooAnimales/Reptil.java
@@ -1,25 +1,66 @@
 package zooAnimales;
 
+import java.util.ArrayList;
+
 public class Reptil extends Animal {
-    private Reptil[] listado;
-    public int iguanas;
-    public int serpientes;
-    private String colorEscamas;
-    private int largoCola;
+    private static ArrayList<Reptil> listado=new ArrayList<>();
+	public static int iguanas;
+	public static int serpientes;
+	private String colorEscamas;
+	private int largoCola;
+	
+	public Reptil() {
+		this(null,0,null,null,null,0);
+	}
+	
+	public Reptil(String nombre, int edad, String habitat, String genero, String colorEscamas, int largoCola) {
+		super(nombre,edad,habitat,genero);
+		this.colorEscamas=colorEscamas;
+		this.largoCola=largoCola;
+		Reptil.listado.add(this);
+		
+	}
+	
+	public static void setListado(ArrayList<Reptil> listado) {
+		Reptil.listado=listado;
+	}
+	public static ArrayList<Reptil> getListado() {
+		return listado;
+	}
+	
+	public void setColorEscamas(String colorEscamas) {
+		this.colorEscamas=colorEscamas;
+	}
+	public String getColorEscamas() {
+		return colorEscamas;
+	}
+	
+	public void setLargoCola(int largoCola) {
+		this.largoCola=largoCola;
+	}
+	public int getLargoCola() {
+		return largoCola;
+	}
+	
+	public int cantidadReptiles() {
+		return Reptil.listado.size();
+		
+	}
+	
 
-    public void cantidadReptiles(){
+	
+	public static Reptil crearIguana(String nombre, int edad, String genero) {
+		Reptil Reptil= new Reptil(nombre, edad, "humedal",genero, "verde",3);
+		Reptil.iguanas++;
+		return Reptil;
+		
+	}
+	
+	public static Reptil crearSerpiente(String nombre, int edad, String genero) {
+		Reptil Reptil =new Reptil(nombre, edad, "jungla", genero, "blanco", 1);
+		Reptil.serpientes++;
+		return Reptil;
+		
+	}
 
-    }
-    public void movimiento(){
-
-    }
-
-    public void crearIguana(){
-
-    }
-    public void crearSerpientes(){
-        
-    }
-
-    
 }
